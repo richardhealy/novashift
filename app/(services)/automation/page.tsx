8ae8862294d8af2f49abf3a4ed6f5cca217d3c94
llreebor <timoschuk.roman@gmail.com>
2025-11-03 14:53:20 +0200

    add automation page hero

--- app/(services)/automation/page.tsx
+++ app/(services)/automation/page.tsx
@@ -1,14 +1,13 @@
 import Cta from "@/components/cta"
 import AutomateSection from "./sections/automate"
 import BenefitsSection from "./sections/benefits"
+import HeroSection from "./sections/hero"
 import SupportSection from "./sections/support"
 
 export default function AutomationPage() {
 	return (
 		<>
-			<div className='h-[400px] w-full bg-neutral-900 text-white text-center text-2xl flex items-center justify-center'>
-				HERO
-			</div>
+			<HeroSection />
 			<BenefitsSection />
 			<AutomateSection />
 			<SupportSection />
---
+++ app/(services)/automation/sections/hero.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image"
+import { TypographyH1 } from "@/components/ui/typography"
+
+export default function HeroSection() {
+	return (
+		<section className='bg-neutral-50 -mt-[58px] overflow-hidden md:mt-[-92px]'>
+			<div className='container'>
+				<div className='relative flex flex-col pt-[102px] pb-[400px] md:flex-row md:pt-[237px] md:pb-[237px] xl:pt-[262px] xl:pb-[200px]'>
+					<div className='md:max-w-[570px]'>
+						<TypographyH1 className='text-center leading-[1.4] md:text-left md:leading-[1.2]!'>
+							Workflow Automations
+						</TypographyH1>
+						<p className='text-neutral-700 mt-8 text-center text-xl font-medium tracking-[0.2px] md:text-left'>
+							Scalable Solutions Built by AI Specialists
+						</p>
+					</div>
+
+					<div className='absolute shrink-0 max-w-[452px] mx-auto -left-[30px] bottom-0 -right-[30px] md:left-[unset] mx:m-0 md:max-w-[400px] lg:max-w-[500px] md:-right-6 md:-bottom-10 lg:h-auto xl:-top-6 xl:-right-14 xl:max-w-[778px]'>
+						<Image
+							className='aspect-389/379'
+							src={"/automation/hero-image.png"}
+							alt='hero image'
+							width={778}
+							height={758}
+						/>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
+}
